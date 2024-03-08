b59948b40faaa592959058638094de046342f4bd
BuildTools <unconfigured@null.spigotmc.org>
2024-03-09 00:09:09 +0100

    Initial Commit

--- src/main/java/net/davido152/minecraftsurvivalascended/MinecraftSurvivalAscended.java
+++ src/main/java/net/davido152/minecraftsurvivalascended/MinecraftSurvivalAscended.java
@@ -16,10 +16,8 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MinecraftSurvivalAscended.MOD_ID)
 public class MinecraftSurvivalAscended {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "mc_sa";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public MinecraftSurvivalAscended() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
